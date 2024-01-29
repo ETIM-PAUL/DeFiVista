a6257d507e5fc5055d33e0df93446efe5dd208de
Mars Ifeanyi <marschalice@gmail.com>
2024-01-29 20:19:31 +0100

    Create Page

---
+++ frontendLite/src/pages/CompanyDetailsPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CompanyDetails from "../component/CompanyDetails";
+
+type Props = {};
+
+const CompanyDetailsPage = (props: Props) => {
+  return (
+    <div>
+      <CompanyDetails />
+    </div>
+  );
+};
+
+export default CompanyDetailsPage;
